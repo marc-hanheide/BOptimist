de46169bb681b5c6d018616aeec38ce0ab677fda
Marc Hanheide <marc@hanheide.de>
2014-09-23 11:33:59 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='boptimist',
+    version='0.1',
+    description='Optimistic Bash executor',
+    license='MIT',
+    author='Marc Hanheide',
+    author_email='marc@hanheide.net',
+    scripts=['boptimist.py']
+)
